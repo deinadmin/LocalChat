d83faa24742c88707cc96deea6cf18fdea74022b
Carl Steen <carldodo@mail.de>
2026-01-19 23:06:48 +0100

    Initial Commit

---
+++ LocalChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalChat
+//
+//  Created by Carl Steen on 19.01.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalChat/LocalChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatApp.swift
+//  LocalChat
+//
+//  Created by Carl Steen on 19.01.26.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
